ff9cc589ebb6bc8042b381f309b98e56c099e9de
vbegun <vbegun@IT-BV.interfarmax.local>
2019-04-03 15:11:18 +0300

    1

---
+++ test_jenkins/src/main/java/test_jenkins/main/AppController.java
@@ -0,0 +1,35 @@
+package test_jenkins.main;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Controller
+@EnableWebMvc
+
+public class AppController {
+
+	
+	@RequestMapping("/")
+    public String index(HttpSession session,
+    		Model model,
+    		@RequestParam(value = "logout",	required = false) String logout){
+		
+		if (logout != null) {
+			SecurityContextHolder.clearContext();
+			session.invalidate();
+			
+			return "redirect:login";
+		}
+
+		
+       // System.out.println("");
+        return "redirect:ldap/user/edit";
+    }
+
+}
