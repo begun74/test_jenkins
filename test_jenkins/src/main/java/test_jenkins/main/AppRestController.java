45d183fcf23897700262cff74f4eb8ee89d443f8
vbegun <vbegun@IT-BV.interfarmax.local>
2019-04-08 10:49:53 +0300

    1

---
+++ test_jenkins/src/main/java/test_jenkins/main/AppRestController.java
@@ -0,0 +1,13 @@
+package test_jenkins.main;
+
+import org.springframework.context.annotation.Import;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@RestController
+@EnableWebMvc
+public class AppRestController {
+	
+//
+}
